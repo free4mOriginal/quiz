ce7d29c856eb6648fad0ffa35e7ed93e0d0d6026
free4mOriginal <free4m@gmail.com>
2020-07-17 12:46:49 -0500

    ADD: async fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
 import React, { useState } from "react";
+import { fetchQuizQuestions, Difficulty, QuestionState } from "./API";
 import QuestionCard from "./components/QuestionCard";
 
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
+
+const TOTAL_QUESTIONS = 10;
+
 const App = () => {
-  const startTrivia = async () => {};
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  console.log(questions);
 
-  const nextQuestion = () => {};
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  };
+
+  // const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  // const nextQuestion = () => {};
 
   return (
     <div className="App">
@@ -16,7 +49,14 @@ const App = () => {
       </button>
       <p className="score">Score</p>
       <p>Loading questions...</p>
-      <QuestionCard />
+      {/* <QuestionCard
+        questionNumber={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      /> */}
       <button className="next">Next question</button>
     </div>
   );
